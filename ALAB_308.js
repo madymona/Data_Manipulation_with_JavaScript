94bd98bb2ed670feba3fba4fc62a991684a4bd4f
madymona <madymona@gmail.com>
2024-05-21 11:55:24 -0400

    Initial push

---
+++ ALAB_308.js
@@ -0,0 +1,8 @@
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+
+const divisibleByFive = (n1 % 5 == 0) && (n2 % 5 == 0) && (n3 % 5 == 0) && (n4 % 5 == 0)
+console.log (divisibleByFive)
